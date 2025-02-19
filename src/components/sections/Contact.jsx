68d770a96beff2cc33dc36ad76a2e66f21dfe779
Iva <iva_dkostadinova@abv.bg>
2025-02-19 23:21:06 +0200

    css fixes

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -33,7 +33,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+      <div className="w-full max-w-lg mx-auto px-6">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             Get in Touch
           </h2>
@@ -45,7 +45,7 @@ export const Contact = () => {
                 name="name"
                 required
                 value={formData.name}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-sm text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="Name..."
                 onChange={(e) =>
                   setFormData({ ...formData, name: e.target.value })
@@ -75,7 +75,7 @@ export const Contact = () => {
                 required
                 value={formData.message}
                 rows={5}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 resize-none"
                 placeholder="Your message..."
                 onChange={(e) =>
                   setFormData({ ...formData, message: e.target.value })
@@ -84,7 +84,7 @@ export const Contact = () => {
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] "
+              className="w-full bg-blue-500 text-white py-2 px-4 text-sm rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] "
             >
               Send message
             </button>
